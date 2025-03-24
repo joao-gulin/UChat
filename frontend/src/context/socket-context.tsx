56786a4161a1d3f14770102f0cdac38c4d24cf94
joao-gulin <joaogulin@gmail.com>
2025-03-24 19:57:21 +0000

    socket context for connection

---
+++ frontend/src/context/socket-context.tsx
@@ -0,0 +1,34 @@
+// src/context/socket-context.tsx
+import { createContext, useContext, useEffect, useState } from "react";
+import { socket } from "../api/socket";
+const SocketContext = createContext({
+  socket: socket,
+});
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    socket.connect();
+
+    const onConnect = () => setIsConnected(true);
+    const onDisconnect = () => setIsConnected(false);
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => useContext(SocketContext);
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,18 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { Routes } from "react-router";
 import { Route } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
